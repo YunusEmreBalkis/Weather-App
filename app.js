8fc0d491c079d0cb82f74f66c3f2506fbe7c0209
YunusEmreBalkis <66324901+YunusEmreBalkis@users.noreply.github.com>
2021-04-10 18:25:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,43 +29,6 @@ function weather(name){
 	  input.value ="";
 
 })	
-
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Roma", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "737b725379msh219e2f7870f346ap11e5bejsn9f752551c1d2",
-// 		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
